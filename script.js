cfea26644b6d9489390d6f57407b3b7885a5162c
zehrasaiyed <158564096+zehrasaiyed@users.noreply.github.com>
2024-02-06 00:45:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ document.querySelector(".close-navbar-icon").addEventListener("click",() =>{
 
 
 
+
 const colors =["#6495ed","#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 
 let i = 0; 
 Array.from(document.querySelectorAll('.nav-link')).forEach((item) => {
     item.style.cssText =`background-color: ${colors[i++]}`
 })
+
+Array.from(document.querySelectorAll('.nav-link')).forEach((item) => {
+    item.addEventListener('click', () => {
+        container.classList.remove("change");
+    });
+});
 
 
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
